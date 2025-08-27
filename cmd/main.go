990c8a2348c7bf2b7486908046f8605f5119a264
Aben <me@aben.io>
2025-08-27 12:38:47 +0800

    init

--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/rushairer/gouno-cli/cmd/gouno"
-
-func main() {
-	gouno.Execute()
-}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rushairer/gouno-cli/gouno"
+
+func main() {
+	gouno.Execute()
+}
