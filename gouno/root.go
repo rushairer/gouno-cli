e7a37410dccc46d85d97dfcff217783066ccd117
Aben <me@aben.io>
2025-08-28 10:51:15 +0800

    feat: comment

--- gouno/root.go
+++ gouno/root.go
@@ -8,12 +8,14 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "gouno",
-	Short: "go-uno is a tool to generate go code",
-	Long: `go-uno is a tool to generate go code.
+	Use:   "gouno-cli",
+	Short: "gouno-cli is a tool to generate go code",
+	Long: `gouno-cli is a tool to generate go code.
 It can generate go code from proto file.`,
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
 }
-
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
